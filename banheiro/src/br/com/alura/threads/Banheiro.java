5161f5119377a6eec1e5b64cd2f2a12f16498da9
Mateus Castro <matcastro>
2018-10-25 00:17:15 -0300

    Aula 5 - Notificação e Daemon threads.

--- banheiro/src/br/com/alura/threads/Banheiro.java
+++ banheiro/src/br/com/alura/threads/Banheiro.java
@@ -1,12 +1,18 @@
 package br.com.alura.threads;
 
 public class Banheiro {
+
+	private boolean ehSujo = true;
+
 	public void fazNumero1() {
 
 		String name = Thread.currentThread().getName();
 		System.out.println(name + " batendo na porta");
 		synchronized (this) {
 			System.out.println(name + " entrando no banheiro");
+			while (ehSujo) {
+				esperaLaFora(name);
+			}
 			System.out.println(name + " fazendo coisa rapida");
 
 			try {
@@ -15,6 +21,7 @@ public class Banheiro {
 				e.printStackTrace();
 			}
 
+			ehSujo = true;
 			System.out.println(name + " dando descarga");
 			System.out.println(name + " lavando a mao");
 			System.out.println(name + " saindo do banheiro");
@@ -27,6 +34,9 @@ public class Banheiro {
 		System.out.println(name + " batendo na porta");
 		synchronized (this) {
 			System.out.println(name + " entrando no banheiro");
+			while (ehSujo) {
+				esperaLaFora(name);
+			}
 			System.out.println(name + " fazendo coisa demorada");
 
 			try {
@@ -35,9 +45,49 @@ public class Banheiro {
 				e.printStackTrace();
 			}
 
+			ehSujo = true;
 			System.out.println(name + " dando descarga");
 			System.out.println(name + " lavando a mao");
 			System.out.println(name + " saindo do banheiro");
+		}
+	}
+
+	public void limpa() {
+
+		String nome = Thread.currentThread().getName();
+
+		System.out.println(nome + " batendo na porta");
+
+		synchronized (this) {
+
+			System.out.println(nome + " entrando no banheiro");
+
+			if (!this.ehSujo) {
+				System.out.println(nome + ", não está sujo, vou sair");
+				return;
+			}
+
+			System.out.println(nome + " limpando o banheiro");
+			this.ehSujo = false;
+
+			try {
+				Thread.sleep(13000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			this.notifyAll();
+
+			System.out.println(nome + " saindo do banheiro");
+		}
+	}
+
+	private void esperaLaFora(String nome) {
+		System.out.println(nome + " eca, banheiro está sujo");
+		try {
+			this.wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 
